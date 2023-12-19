a149e2a201bbb70146e8df207764a6e0e07dbb4f
felix blanc <felix.blanc@epita.fr>
2023-12-19 14:35:39 +0100

    New test for correcting commit :)

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.URI;
+
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
